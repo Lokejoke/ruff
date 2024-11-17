d9c87e2fab84a5d7228dc6f73d9e0652ae193170
xbrtnik1 <xbrtnik1@mail.muni.cz>
2024-11-17 10:56:47 +0100

    Add always fixable violation attribute

--- crates/ruff_linter/resources/test/fixtures/wps_light/WPS303.py
+++ crates/ruff_linter/resources/test/fixtures/wps_light/WPS303.py
@@ -46,3 +46,21 @@ if 10 .real:
 # This is a type error, not a syntax error
 y = 100[no]
 y = 100(no)
+
+bin = 0b1001_1010_0001_0100
+hex = 0x1b_a0_44_fe
+dec = 33_554_432
+real = 1_000.111_1e-1_000
+
+valid = 0_0_0
+also_ok = 000
+4_2
+1_0000_0000
+0b1001_0100
+0xffff_ffff
+0o5_7_7
+1_00_00.5
+1e1_0
+.1_4
+0x_f
+0o_5
--- crates/ruff_linter/src/rules/wps_light/rules/underscores_in_number.rs
+++ crates/ruff_linter/src/rules/wps_light/rules/underscores_in_number.rs
@@ -1,4 +1,4 @@
-use ruff_diagnostics::{Diagnostic, Violation};
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::ExprNumberLiteral;
 use ruff_text_size::Ranged;
@@ -6,23 +6,41 @@ use ruff_text_size::Ranged;
 use crate::checkers::ast::Checker;
 
 /// ## What it does
+/// Forbids underscores (`_`) in number literals.
 ///
 /// ## Why is this bad?
+/// Numbers like `1000` can be written in multiple ways using underscores:
+/// `1_000`, `10_00`, and `100_0`. While all of these are valid and represent
+/// the same number, they rely on the author's cultural habits, leading to
+/// inconsistencies and potential confusion. Enforcing a single, clear way to
+/// write numbers improves readability and maintainability.
 ///
 /// ## Example
 /// ```python
+/// let phone = 8_83_134_43
+/// let million = 100_00_00
 /// ```
 ///
 /// Use instead:
 /// ```python
+/// let phone = 88313443;
+/// let million = 1000000;
 /// ```
-#[violation]
-pub struct UnderscoresInNumber;
 
-impl Violation for UnderscoresInNumber {
+#[violation]
+pub struct UnderscoresInNumber {
+    number: String,
+}
+
+impl AlwaysFixableViolation for UnderscoresInNumber {
     #[derive_message_formats]
     fn message(&self) -> String {
-        "Number is separated in ".to_string()
+        let Self { number, .. } = self;
+        format!("Found underscores in number literal `{number}`")
+    }
+
+    fn fix_title(&self) -> String {
+        "Remove underscores from number literal".to_string()
     }
 }
 
@@ -32,9 +50,16 @@ pub(crate) fn underscores_in_number(checker: &mut Checker, number: &ExprNumberLi
     if !num_str.contains('_') {
         return;
     }
-    let diagnostic = Diagnostic::new(
-        UnderscoresInNumber,
-        number.range(),
+    checker.diagnostics.push(
+        Diagnostic::new(
+            UnderscoresInNumber {
+                number: num_str.to_string(),
+            },
+            number.range(),
+        )
+        .with_fix(Fix::safe_edit(Edit::range_replacement(
+            num_str.replace('_', ""),
+            number.range(),
+        ))),
     );
-    checker.diagnostics.push(diagnostic);
 }
