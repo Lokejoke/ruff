270efacc7d069a7dfee4e8348c7bed9f35ae8728
xbrtnik1 <xbrtnik1@mail.muni.cz>
2024-11-17 09:23:16 +0100

    Added more complex tests, corrected logic

--- crates/ruff_linter/src/rules/wps_light/rules/in_compare_with_single_item_container.rs
+++ crates/ruff_linter/src/rules/wps_light/rules/in_compare_with_single_item_container.rs
@@ -1,8 +1,5 @@
 use std::iter::zip;
 
-use ruff_python_semantic::analyze::typing::find_binding_value;
-use ruff_python_semantic::{BindingId, SemanticModel};
-
 use ruff_diagnostics::{Diagnostic, Violation};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_python_ast::ExprCompare;
@@ -35,7 +32,7 @@ pub struct InCompareWithSingleItemContainer;
 impl Violation for InCompareWithSingleItemContainer {
     #[derive_message_formats]
     fn message(&self) -> String {
-        "Avoid comparing to a single item container with `in` operator".to_string()
+        "Avoid comparing to a single item container with `in` or `not in` operator".to_string()
     }
 }
 
@@ -44,16 +41,11 @@ pub(crate) fn in_compare_with_single_item_container(checker: &mut Checker, compa
     let ExprCompare {
         ops, comparators, ..
     } = compare;
-    let semantic = checker.semantic();
     let diagnostics: Vec<Diagnostic> = zip(ops, comparators)
         .filter(|(op, comparator)| {
-            // Check if the operator is CmpOp::In or CmpOp::NotIn
-            matches!(op, CmpOp::In | CmpOp::NotIn)
-                // Or check if the comparator is a single item container
-                || is_single_item_container(semantic, comparator)
+            matches!(op, CmpOp::In | CmpOp::NotIn) && is_single_item_container(comparator)
         })
         .map(|(_, comparator)| {
-            // Create a diagnostic for each match
             Diagnostic::new(InCompareWithSingleItemContainer, comparator.range())
         })
         .collect();
@@ -62,22 +54,22 @@ pub(crate) fn in_compare_with_single_item_container(checker: &mut Checker, compa
     checker.diagnostics.extend(diagnostics);
 }
 
-fn is_single_item_container(semantic: &SemanticModel, expr: &Expr) -> bool {
+fn is_single_item_container(expr: &Expr) -> bool {
     match expr {
-        Expr::Dict(expr_dict) => expr_dict.len() == 1,
-        Expr::Set(expr_set) => expr_set.len() == 1,
-        Expr::Name(expr_name) => {
-            let scope = semantic.current_scope();
-            let bindings: Vec<BindingId> = scope.get_all(expr_name.id()).collect();
-            let [binding_id] = bindings.as_slice() else {
+        Expr::Dict(container) => container.len() == 1,
+        Expr::Set(container) => {
+            if container.len() != 1 {
                 return false;
-            };
-            let binding = semantic.binding(*binding_id);
-            find_binding_value(binding, semantic)
-                .is_some_and(|value| is_single_item_container(semantic, value))
+            }
+            !container.elts[0].is_starred_expr()
         }
-        Expr::List(expr_list) => expr_list.len() == 1,
-        Expr::Tuple(expr_tuple) => expr_tuple.len() == 1,
+        Expr::List(container) => {
+            if container.len() != 1 {
+                return false;
+            }
+            !container.elts[0].is_starred_expr()
+        }
+        Expr::Tuple(container) => container.len() == 1,
         _ => false,
     }
 }
