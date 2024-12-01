d0b20184ba79bfda32aa96e12ca08bcf9c2f1cf6
Lokejoke <lokejoke1@gmail.com>
2024-12-01 17:15:42 +0100

    idc

--- crates/ruff_linter/src/rules/wps_light/rules/mod.rs
+++ crates/ruff_linter/src/rules/wps_light/rules/mod.rs
@@ -1,5 +1,4 @@
 pub(crate) use consecutive_underscores_in_name::*;
-pub(crate) use use_of_unused_marked_variables::*;
 pub(crate) use list_multiplication::*;
 pub(crate) use reserved_argument_as_variable::*;
 pub(crate) use underscores_in_number::*;
@@ -7,7 +6,6 @@ pub(crate) use assignment_to_subscript_slice::*;
 pub(crate) use getter_without_return_value::*;
 
 mod consecutive_underscores_in_name;
-mod use_of_unused_marked_variables;
 mod list_multiplication;
 mod reserved_argument_as_variable;
 mod underscores_in_number;
